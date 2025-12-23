73c4c1e4078e979a421a36828953d205527eb391
sebastian-enz <5671344+sebastian-enz@users.noreply.github.com>
2025-12-23 23:33:15 +0100

    Update Config.java

--- src/main/java/org/markomannia/mw2d/Config.java
+++ src/main/java/org/markomannia/mw2d/Config.java
@@ -4,9 +4,9 @@ import java.util.Base64;
 
 public class Config {
 
-	public static final String BASE_PATH = "/Users/uwol/github/kstvmarkomannia/markomannenwiki/docs";
+	public static final String BASE_PATH = "/workspaces/mediawiki2docusaurus/out";
 
-	public static final String MEDIAWIKI_URL = "https://old.markomannenwiki.de";
+	public static final String MEDIAWIKI_URL = "https://support.xtento.com/wiki/";
 
 	public static final String OPTIONAL_AUTH = Base64.getEncoder().encodeToString("username:password".getBytes());
 }
